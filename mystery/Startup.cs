23d8170bca6b0c933ba7518f3701ddde7d9ba8b0
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-03-26 16:01:52 -0600

    Startup is setup

--- mystery/Startup.cs
+++ mystery/Startup.cs
@@ -74,13 +74,20 @@ namespace mystery
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "mystery v1"));
+                app.UseCors("CorsDevPolicy");
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
+
+            app.UseDefaultFiles();
+
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
